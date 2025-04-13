9c07ab2599898b5b78804d515a0998a3e51a7d57
Okai05 <kaikaioct5@gmail.com>
2025-04-13 00:27:53 -0700

    All the code for the nodes
    All the nodes now have all the code they need and now all the nodes just need to be adjusted

---
+++ scenes/battle/deck.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+const CARD_SCENE_PATH = "res://scenes/battle/card.gd"
+const CARD_DRAW_SPEED = 1
+
+var player_deck = ["Knight", "Knight", "Knight"]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$RichTextLabel.text = str(player_deck.size())
+
+
+
+func draw_card():
+	var card_drawn = player_deck[0]
+	player_deck.erase(card_drawn)
+	
+	#If player drew the last card in teh deck, disable the deck
+	if player_deck.size() == 0:
+		$Area2D/CollisionShape2D.disabled = true
+		$Sprite2D.visible = false
+		$RichTextLabel.visible = false
+	
+	$RichTextLabel.text = str(player_deck.size())
+	
+	var card_scene = preload(CARD_SCENE_PATH)
+	var new_card = card_scene.instantiate()
+	$"../CardManager".add_child(new_card)
+	new_card.name = "Card"
+	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
