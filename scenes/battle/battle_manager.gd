7fc25906150f17ffcbc2efc681e5cab6116f7421
Okai05 <kaikaioct5@gmail.com>
2025-04-13 22:02:50 -0700

    Boss Health and Damage
    What is now needed is a way for the player to attack by clicking on their cards

--- scenes/battle/battle_manager.gd
+++ scenes/battle/battle_manager.gd
@@ -1,20 +1,32 @@
 extends Node
 
+const STARTING_HEALTH = 10
+
 var max_cool_down := 4
 var curr_cool_down := 4
 var opponent_health = 10
 var player_cards_on_battlefield = []
 
-var player_health = 3
+var player_health
+var boss_health
+var boss_damage = 2
 var boss1_stats = {"Vacuum": {"HP": 10, "Attack": 2, "Block": 2, "Kill": 10}}
-var monster_cards = {"Sandwich Knight": {"HP":5, "Attack": 1}, "Pizza Ninja": {"HP":5, "Attack": 1} }
-
+var monster_cards = {"Sandwich": {"HP":5, "Attack": 1}, "Pizza": {"HP":5, "Attack": 1} }
 
 @onready var battle_timer: Timer = $"../BattleTimer"
 @onready var cardslot_1: Node2D = $"../Cardslot"
 @onready var cardslot_2: Node2D = $"../Cardslot2"
 @onready var cardslot_3: Node2D = $"../Cardslot3"
 
+func _ready() ->void:
+	player_health = STARTING_HEALTH
+	$"../PlayerHealth".text = str(player_health)
+	
+	boss_health = STARTING_HEALTH
+	$"../BossHealth".text = str(boss_health)
+	
+	$"../BossAttack".text = str(boss_damage)
+	
 
 func _on_end_turn_button_pressed() -> void:
 	opponent_turn()
@@ -46,11 +58,16 @@ func opponent_turn():
 	await battle_timer.timeout
 	
 	# End Turn
-	end_opponent_turn()
 	start_player_turn()
 
+
 func start_player_turn():
-	#maybe whenever card is played add it to an dictionary so that the new dictionary
+	$"../CardManager".reset_played()
+	$"../Deck".reset_draw()
+	$"../EndTurnButton".disabled = false
+	$"../EndTurnButton".visible = true
+	
+		#maybe whenever card is played add it to an dictionary so that the new dictionary
 	#can keep track of the cards on the field rather than the original
 	#I still do not know how to copy a variable from another dictionary into a new dictionary -kai
 	#the dictionary would look something like this
@@ -64,14 +81,7 @@ func start_player_turn():
 	#(possibly by storing the cardslots that have already attacked into an array and 
 	#preventing them from being played again)
 	
-	
-	opponent_turn()
-
-func end_opponent_turn():
-	$"../CardManager".reset_played()
-	$"../Deck".reset_draw()
-	$"../EndTurnButton".disabled = false
-	$"../EndTurnButton".visible = true
+	#create an if function that checks boss's health at the end of the turn as if it is zero then the player has won
 
 
 
@@ -93,8 +103,13 @@ func opponent_move():
 
 
 func choose_target() -> Cardslot:
+	#for now this is how it will target cards
 	if cardslot_1.card_in_slot:
 		return cardslot_1
+	elif cardslot_2.card_in_slot:
+		return cardslot_2
+	elif cardslot_3.card_in_slot:
+		return cardslot_3
 	
 	# Calculate turn numbers needed to kill player
 	# and that the cards can kill the enemy
@@ -106,14 +121,20 @@ func choose_target() -> Cardslot:
 func opponent_attack(target, attacker):
 	if attacker == "boss":
 		var boss_attack = boss1_stats["Vacuum"]["Attack"]
+		player_health = max(0, player_health - boss_attack)
+		$"../PlayerHealth".text = str(player_health)
 		print("Opponent Attack")
 	elif attacker == "player":
 		var player_attack = monster_cards[attacker]["Attack"]
+		#attacker needs to reference the cards in CardDatabase.gd
+		boss_health = max(0, boss_health - player_attack)
+		$"../BossHealth".text = str(boss_health)
 		print("Player Attack")
 
 
 func opponent_defend():
-	boss1_stats["Vacuum"]["HP"] +=2
+	boss_health = max(boss_health + 2, 10)
+	$"../BossHealth".text = str(boss_health)
 	print("Opponent Defend")
 
 
