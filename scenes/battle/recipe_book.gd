149be91026c81333206bba68e2e2fd8e8fcde2e5
MOBIUS <yuchenliu0710@outlook.com>
2025-04-29 21:53:37 -0700

    adjust monster card health and attack font size to 16

--- scenes/battle/recipe_book.gd
+++ scenes/battle/recipe_book.gd
@@ -6,6 +6,10 @@ func _ready() -> void:
 	recipe_book_visible.visible = false
 
 func _on_recipe_book_button_pressed() -> void:
+	if not recipe_book_visible:
+		print("NO")
+	else:
+		print("YES")
 	recipe_book_visible.visible = true
 	
 func _on_exit_book_pressed() -> void:
