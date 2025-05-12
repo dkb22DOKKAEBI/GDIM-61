6c2e2a4c96c0b5643d7bc74205ca9de7315c67ec
dkb22DOKKAEBI <leo.chang636@gmail.com>
2025-05-12 00:56:52 -0700

    Deckhand changes

--- scripts/button/switch_hand.gd
+++ scripts/button/switch_hand.gd
@@ -3,7 +3,6 @@ extends Node
 @export var ingredient_card_manager: Node2D
 @export var monster_card_manager: Node2D
 
-
 func _on_switch_hand() -> void:
 	# Update whether on ingredient hand in PlayerHand script
 	PlayerHand.on_ingredient_hand = !PlayerHand.on_ingredient_hand
@@ -15,3 +14,8 @@ func _on_switch_hand() -> void:
 	monster_card_manager.visible = !PlayerHand.on_ingredient_hand
 	for card in monster_card_manager.get_children():
 		card.get_child(1).get_child(0).disabled = int(PlayerHand.on_ingredient_hand)
+	
+	if $"../../HandUI/RichTextLabel".text == "Ingredients":
+		$"../../HandUI/RichTextLabel".text = "Monsters"
+	else:
+		$"../../HandUI/RichTextLabel".text = "Ingredients"
