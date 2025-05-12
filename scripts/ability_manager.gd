e6c5e701a02629ab4ea8f4b09ace74e96688f570
Okai05 <kaikaioct5@gmail.com>
2025-05-12 04:00:21 -0700

    Card Ability Tracker
    this change now makes it so that when a card is placed its cooldown is tracked and also it will add an ability card to your hand but the ability card acts as a monster card for now.

---
+++ scripts/ability_manager.gd
@@ -0,0 +1,55 @@
+extends Node
+class_name Ability
+
+@export var cap_ability :bool
+@export var monster_name : String
+
+#the dictionary will be "monster name": [do they have an ability, what type]
+var monster_abilities = {
+	"Pizza":[true, "damage"],
+	"Cheesecake": [false],
+	"Sandwich": [true, "block"],
+	"Quesadilla": [true, "heal"],
+	"Salad": [true, "heal"],
+	"Taco": [false],
+	"Trashcan": [false]
+	}
+
+
+
+func add_ability_card(monster_name):
+	if not monster_abilities.has(monster_name):
+		return null  # monster isn't even in the table
+
+	if monster_abilities[monster_name][0] == false:
+		return null  # monster has no ability, exit early
+
+	var ability_type = monster_abilities[monster_name][1]
+
+	match ability_type:
+		"damage":
+			return check_damage_ability(monster_name)
+		"block":
+			return check_block_ability(monster_name)
+		"heal":
+			return check_heal_ability(monster_name)
+		_:
+			return null  # unknown ability type
+
+
+func check_damage_ability(name):
+	match name:
+		"Pizza":
+			return ["Attack", 3]
+
+func check_heal_ability(name):
+	match name:
+		"Quesadilla":
+			return ["Heal", 2]
+		"Salad":
+			return ["Heal", 1]
+
+func check_block_ability(name):
+	match name:
+		"Sandwich":
+			return ["Block", 1]
--- scripts/card/cardslot.gd
+++ scripts/card/cardslot.gd
@@ -1,8 +1,11 @@
 class_name Cardslot
 extends Node2D
 
+const CARDSLOT_MANAGER_PATH = "res://scripts/cardslot_manager.gd"
 var card_in_slot: Card = null
-
+@export var card_slot_number : String
 
 func _ready():
 	CardslotManager.cardslots.append(self)
+	print(card_slot_number)
+	print(1)
