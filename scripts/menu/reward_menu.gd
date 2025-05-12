cde985c89fb9a067b988ba1af2b33687bcf4530b
MOBIUS <yuchenliu0710@outlook.com>
2025-05-12 08:02:08 -0700

    add claim reward feature

--- scripts/menu/reward_menu.gd
+++ scripts/menu/reward_menu.gd
@@ -1,7 +1,23 @@
 extends Node
 
+const ingredient_reward: Array[String] = ["Cheese", "Tomato", "Cheese", "Tomato", "Dough"]
+const monster_reward: Array[String] = ["Pizza"]
+
 
 # Attached to Next Level button
 # Proceed to the next level
 func on_next_level() -> void:
 	SceneManager.proceed_to_next_level()
+
+
+# Attached to Claim Reward button
+# Claim reward from completion of previous level
+func on_claim_reward() -> void:
+	# Add ingredient reward to player's deck
+	for reward_ingredient: String in ingredient_reward:
+		PlayerController.deck.append(reward_ingredient)
+	PlayerController.deck.shuffle()
+	
+	# Add monster reward to player's hand
+	for reward_monster: String in monster_reward:
+		PlayerHand.legacy_monster_hand.append(reward_monster)
