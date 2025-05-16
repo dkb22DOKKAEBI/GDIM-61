96c4658fb93558ba70c4080678724a8f2511abfc
MOBIUS <yuchenliu0710@outlook.com>
2025-05-15 23:26:47 -0700

    add monster card goes up when hovered over functionality

--- scripts/card_manager/card_manager.gd
+++ scripts/card_manager/card_manager.gd
@@ -10,6 +10,8 @@ var card_being_dragged # Card type
 var screen_size
 #var is_hovering_on_card
 @export var temp_ui: Control
+@export var cover: ColorRect
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready()->void:
@@ -32,6 +34,9 @@ func on_hovered_over_card(card):
 			if ingredient_card.scale.x == 1.2:
 				highlight_card(ingredient_card, false)
 			ingredient_card.set_pickable(false)
+		
+		# Make cover for ingredient cards visible
+		cover.visible = true
 	
 	highlight_card(card, true)
 	
@@ -48,6 +53,9 @@ func on_hovered_off_card(card):
 		# Enable all detection for ingredient cards hover
 		for ingredient_card in PlayerHand.player_ingredient_hand:
 			ingredient_card.set_pickable(true)
+		
+		# Make cover for ingredient cards invisible
+		cover.visible = false
 	
 	highlight_card(card, false)
 	
@@ -60,7 +68,19 @@ func highlight_card(card, hovered):
 		#card.scale = Vector2(1.25, 1.25)
 		card.scale *= 1.2
 		card.set_card_z_index(2)
+		
+		# Move up monster card
+		if card.is_monster_card:
+			var new_position: Vector2 = card.position
+			new_position.y -= PlayerHand.MONSTER_CARD_Y_OFFSET
+			PlayerHand.animate_card_to_position(card, new_position, 0.1)
 	else:
 		#card.scale = Vector2(1, 1)
 		card.scale /= 1.2
 		card.set_card_z_index(0)
+		
+		# Move down monster card
+		if card.is_monster_card:
+			var new_position: Vector2 = card.position
+			new_position.y += PlayerHand.MONSTER_CARD_Y_OFFSET
+			PlayerHand.animate_card_to_position(card, new_position, 0.1)
