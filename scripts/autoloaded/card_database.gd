b64f2667f80ac3e3b921d2dc2244e3188c720c73
MOBIUS <yuchenliu0710@outlook.com>
2025-05-19 03:18:21 -0700

    add tutorial boss in the scene

--- scripts/autoloaded/card_database.gd
+++ scripts/autoloaded/card_database.gd
@@ -24,7 +24,9 @@ const BOSS_LEVEL = {
 
 # Path to boss scenes
 const VACUUM_SCENE_PATH = "res://scenes/boss/vacuum.tscn"
+const LIGHTBULB_SCENE_PATH = "res://scenes/boss/lightbulb.tscn"
 const BOSS_PATH = {
+	"Lightbulb": LIGHTBULB_SCENE_PATH,
 	"Vacuum": VACUUM_SCENE_PATH
 }
 
--- scripts/tutorial/tutorial_start.gd
+++ scripts/tutorial/tutorial_start.gd
@@ -6,15 +6,15 @@ extends Node
 
 func _ready() -> void:
 	# Instantiate boss
-	#var boss_name = CardDatabase.BOSS_LEVEL[SceneManager.level_index]
-	#var boss_scene = load(CardDatabase.BOSS_PATH[boss_name])
-	#var boss: Node2D = boss_scene.instantiate()
-	#boss.boss_name = boss_name
-	#
-	## Add boss as child of Enemy node
-	#boss_parent.add_child(boss)
-	#
-	#boss.battle_manager = $"../BattleManager"
+	var boss_name = "Lightbulb"
+	var boss_scene = load(CardDatabase.BOSS_PATH[boss_name])
+	var boss: Node2D = boss_scene.instantiate()
+	boss.boss_name = boss_name
+	
+	# Add boss as child of Enemy node
+	boss_parent.add_child(boss)
+	
+	boss.battle_manager = $"../BattleManager"
 	
 	# Start Tutorial
 	tutorial_prep.curr_message.activate_self()
