2c138e3dec1b63a2500c7dd562eb08920e059998
MOBIUS <yuchenliu0710@outlook.com>
2025-05-18 23:39:02 -0700

    set up tutorial message

---
+++ scripts/autoloaded/event_controller.gd
@@ -0,0 +1,3 @@
+extends Node
+
+signal preceed_tutorial_signal(next_message: TutorialMessage)
--- scripts/tutorial/tutorial_prep.gd
+++ scripts/tutorial/tutorial_prep.gd
@@ -2,13 +2,21 @@ extends Node
 
 const TUTORIAL_DECK: Array[String] = ["Tomato", "Dough", "Cheese", "Lettuce",
 "Tomato", "Sugar", "Mystery_Meat", "Lettuce", "Tortilla", "Dough"]
+@export var curr_message: TutorialMessage
 
 
 # Ready
 func _ready():
-	print("Tutorial Prep")
+	# Connect signals
+	EventController.connect("preceed_tutorial_signal", update_curr_message)
+	
 	# Update player setup for tutorial
 	PlayerHand.legacy_monster_hand.clear()
 	PlayerController.deck = TUTORIAL_DECK.duplicate()
 	
 	# Disable buttons
+
+
+# Update current tutorial message
+func update_curr_message(next_message: TutorialMessage):
+	curr_message = next_message
