e5f5f57fc80d9398f2648002f63ff560ec4011a5
dkb22DOKKAEBI <leo.chang636@gmail.com>
2025-05-27 00:24:35 -0700

    audio fix

--- global/audio_manager.gd
+++ global/audio_manager.gd
@@ -7,13 +7,14 @@ extends Node
 
 
 # Play sound
-func play_sound(sfx_name: String) -> void:
+func play_sound(sfx_name: String, sfx_db: float = 0) -> void:
 	# Check whether has given sound effect
 	if sfx_dic.has(sfx_name): # Does have sfx
 		# Create new sound
 		var new_2d_audio = AudioStreamPlayer2D.new()
 		self.add_child(new_2d_audio)
 		new_2d_audio.stream = sfx_dic[sfx_name]
+		new_2d_audio.volume_db = sfx_db
 		new_2d_audio.finished.connect(new_2d_audio.queue_free) # Destroy when finish playing
 		
 		new_2d_audio.play()
