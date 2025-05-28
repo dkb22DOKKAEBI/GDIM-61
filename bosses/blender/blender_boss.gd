7e4d13e201bb002eab365da37303ec2d0e332cad
MOBIUS <yuchenliu0710@outlook.com>
2025-05-27 22:53:51 -0700

    add in blender scene and scripts

---
+++ bosses/blender/blender_boss.gd
@@ -0,0 +1,23 @@
+class_name Blender
+extends Boss
+
+
+# Initialization of boss stats
+func _ready():
+	# Boss basic stats
+	super._ready()
+	
+	# Boss unique stats
+
+
+# Boss behavior logic
+func on_action() -> void:
+	super.on_action()
+
+
+# Boss abilities
+# Ability 1: 
+
+# Ability 2:
+
+# Ability 3:
