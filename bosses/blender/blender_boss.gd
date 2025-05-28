92d5c259985746e4387f4e2646d187c0e75eabb1
MOBIUS <yuchenliu0710@outlook.com>
2025-05-27 23:41:30 -0700

    add basic logic for blender

--- bosses/blender/blender_boss.gd
+++ bosses/blender/blender_boss.gd
@@ -1,7 +1,10 @@
 class_name Blender
 extends Boss
 
-var ramp_cool_down: int
+var max_ramp_cool_down: int
+var curr_ramp_cool_down: int
+var ramp_attack: int
+var double_hit_chance: float
 
 
 # Initialization of boss stats
@@ -10,27 +13,60 @@ func _ready():
 	super._ready()
 	
 	# Boss unique stats
-	ramp_cool_down = CardDatabase.BOSS_STATS["Blender"]["RampCoolDown"]
+	max_ramp_cool_down = CardDatabase.BOSS_STATS["Blender"]["RampCoolDown"]
+	curr_ramp_cool_down = max_ramp_cool_down
+	double_hit_chance = CardDatabase.BOSS_STATS["Blender"]["DoubleHitChance"]
+	ramp_attack = CardDatabase.BOSS_STATS["Blender"]["RampAttack"]
 
 
 # Boss behavior logic
 func on_action() -> void:
-	super.on_action()
+	super.on_action()	
 	
 	# Boss action
 	# Check whether has a backline
 	if CardslotManager.cardslots[1].card_in_slot or CardslotManager.cardslots[2].card_in_slot:
-		swap_front_back_line()
+		blender_swap_front_back_line()
 		curr_cool_down = max_cool_down
+	else:
+		blender_attack()
+	
+	# Boss ramping attack
+	if curr_ramp_cool_down == 0:
+		blender_ramp_up_attack()
+		curr_ramp_cool_down = max_ramp_cool_down
+	
 
 
 # Boss abilities
 # Ability 1: Regular attack with chance of doulbe hit
+func blender_attack() -> void:
+	# Choose target
+	var target = choose_target()
+	
+	var old_pos:Vector2 = self.global_position
+	if not target:
+		boss_attack_player_anim()
+		await battle_manager.wait(0.5)
+		battle_manager.player_take_dmg(1)
+	else:
+		boss_attack_monster_anim(target)
+		await battle_manager.wait(0.5)
+		battle_manager.player_cards_on_battlefield[target].take_damage(boss_attack)
+	
+	# Enemy return to original position
+	boss_return_pos_anim(old_pos)
+	
+	# Check whether player lose
+	battle_manager.player_check_dead()
 
 # Ability 2: Ramping up attack
+func blender_ramp_up_attack() -> void:
+	boss_attack += ramp_attack
+	boss_attack_text.text = str(boss_attack)
 
 # Ability 3: Swap frontline with backline
-func swap_front_back_line() -> void:
+func blender_swap_front_back_line() -> void:
 	# Get targeted backline
 	var target_cardslot : Cardslot
 	if CardslotManager.cardslots[1].card_in_slot:
