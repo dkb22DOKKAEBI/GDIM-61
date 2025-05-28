f1f91a5b9ccff14bbd66872024931900f373583d
MOBIUS <yuchenliu0710@outlook.com>
2025-05-27 22:25:45 -0700

    add toaster logic

--- bosses/toaster/toaster_boss.gd
+++ bosses/toaster/toaster_boss.gd
@@ -4,6 +4,7 @@ extends Boss
 
 var spawn_max_cool_down: int
 var curr_spawn_cool_down: int
+var low_hp_line: int = 7
 @export var breadspwan_1: Node2D
 @export var breadspwan_2: Node2D
 
@@ -24,12 +25,27 @@ func _ready():
 # Boss behavior logic
 func on_action() -> void:
 	super.on_action()
+	# Update cool down
+	if curr_spawn_cool_down != 0:
+		curr_spawn_cool_down -= 1
 	
 	# Breadspawn attack
 	if breadspwan_1.get_child_count() != 0:
 		breadspwan_1.get_child(0).breadspawn_attack()
 	if breadspwan_2.get_child_count() != 0:
 		breadspwan_2.get_child(0).breadspawn_attack()
+	
+	# Toaster action
+	if breadspwan_1.get_child_count() == 0 and breadspwan_2.get_child_count() == 0 and curr_spawn_cool_down == 0:
+		spawn_bread()
+		curr_spawn_cool_down = spawn_max_cool_down
+	elif boss_health < low_hp_line and (breadspwan_1.get_child_count() != 0 or breadspwan_2.get_child_count() != 0):
+		if breadspwan_1.get_child_count() != 0:
+			toaster_exchange_health(breadspwan_1.get_child(0))
+		else:
+			toaster_exchange_health(breadspwan_2.get_child(0))
+	else:
+		toaster_attack()
 	
 
 
@@ -72,3 +88,22 @@ func toaster_attack() -> void:
 	
 	# Check whether player lose
 	battle_manager.player_check_dead()
+
+# Ability 3: Exchange breadspawn for health
+func toaster_exchange_health(breadspawn: Breadspawn) -> void:
+	# Restore health
+	boss_health += breadspawn.boss_health
+	boss_health = min(boss_health, CardDatabase.BOSS_STATS["Toaster"]["HP"])
+	boss_health_text.text = str(boss_health)
+	
+	# Change font to double size and green
+	boss_health_text.add_theme_font_size_override("normal_font_size", 40)
+	boss_health_text.modulate = Color.GREEN
+
+	# Play animation for health change
+	var tween = get_tree().create_tween()
+	tween.tween_property(boss_health_text, "theme_override_font_sizes/normal_font_size", 16, 1)
+	tween.tween_property(boss_health_text, "modulate", Color.BLACK, 1)
+	
+	# Delete breadspawn
+	breadspawn.queue_free()
