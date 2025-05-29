96863f2fe9174f1c8757f35cc7fd8ed149cf19aa
MOBIUS <yuchenliu0710@outlook.com>
2025-05-29 11:23:11 -0700

    add in kettle scene and script

---
+++ bosses/kettle/kettle_boss.gd
@@ -0,0 +1,15 @@
+class_name Kettle
+extends Boss
+
+
+# Initialization of boss stats
+func _ready():
+	# Boss basic stats
+	super._ready()
+	
+	# Boss unique stats
+
+
+# Boss behavior logic
+func on_action() -> void:
+	super.on_action()
