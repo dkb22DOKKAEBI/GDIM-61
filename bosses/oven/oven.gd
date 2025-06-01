991c8a4f8e6c263f26c0c148850d37c9e8083865
MOBIUS <yuchenliu0710@outlook.com>
2025-06-01 04:02:40 -0700

    fix oven logic and update its regular attack

--- bosses/oven/oven.gd
+++ bosses/oven/oven.gd
@@ -1,6 +1,8 @@
 class_name Oven
 extends Boss
 
+signal oven_action_finish_signal()
+
 var self_dmg: int
 @onready var new_battle: Node2D = $"."
 @onready var attacksfx = new_battle.get_node("attacksfx")
@@ -18,17 +20,13 @@ func _ready():
 func on_action() -> void:
 	super.on_action()
 	
-	# Choose skills
-	var skill = 0
+	# Choose ability to perform
 	if curr_cool_down == 0:
-		skill = 1
-	
-	match skill:
-		0:
-			oven_attack()
-		1:
-			oven_multi_attack()
-			curr_cool_down = max_cool_down
+		oven_multi_attack()
+		curr_cool_down = max_cool_down
+	else:
+		oven_attack()
+	await oven_action_finish_signal
 	
 	# Self take damage
 	boss_take_dmg(self_dmg)
@@ -37,42 +35,24 @@ func on_action() -> void:
 # Boss abilities
 # Ability 1: Regular attack
 func oven_attack() -> void:
-	# Choose target
+	# Regular attack
 	var target = choose_target()
+	regular_attack(target)
+	await boss_regular_attack_finish_signal
 	
-	var old_pos:Vector2 = battle_manager.enemy.global_position
-	if not target:
-		boss_attack_player_anim()
-		await battle_manager.wait(0.5)
-		battle_manager.player_take_dmg(2)
-	else:
-		boss_attack_monster_anim(target)
-		await battle_manager.wait(0.5)
-		battle_manager.player_cards_on_battlefield[target].take_damage(boss_attack)
-	
-	# Enemy return to original position
-	boss_return_pos_anim(old_pos)
-	
-	# Check whether player lose
-	battle_manager.player_check_dead()
+	# Signal action over
+	oven_action_finish_signal.emit()
 
 # Ability 2: Multiple attacks
 func oven_multi_attack() -> void:
-	# Attack three targets
+	# Attack three times with monsters as priority
 	for i in range(3):
 		var target = multi_attack_choose_target()
-		if not target:
-			battle_manager.player_take_dmg(2)
-		else:
-			battle_manager.player_cards_on_battlefield[target].take_damage(boss_attack)
+		regular_attack(target)
+		await boss_regular_attack_finish_signal
 	
-	# Play attack animation
-	var old_pos:Vector2 = battle_manager.enemy.global_position
-	boss_attack_player_anim()
-	await battle_manager.wait(0.5)
-	
-	# Enemy return to original position
-	boss_return_pos_anim(old_pos)
+	# Signal action over
+	oven_action_finish_signal.emit()
 
 func multi_attack_choose_target() -> Cardslot:
 	if CardslotManager.cardslots[0].card_in_slot:
