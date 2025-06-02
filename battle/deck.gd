9ff26f1906af50e017486833d9d25b9c7f61695d
MOBIUS <yuchenliu0710@outlook.com>
2025-06-01 20:03:33 -0700

    fix bug of game crashing after running out of deck

--- battle/deck.gd
+++ battle/deck.gd
@@ -6,7 +6,6 @@ var CARD_DRAW_SPEED = 1
 var card_starting_position: Vector2 = Vector2(915, 525)
 
 @export var deck_left_num_text: RichTextLabel
-@export var deck_sprite: Sprite2D
 @export var ingredient_card_manager: Node2D
 @onready var drawaudio: AudioStreamPlayer = $"../../drawsfx"
 
@@ -21,8 +20,7 @@ func _ready():
 func draw_card():
 	# If player drew the last card in the deck, disable the deck
 	if PlayerController.deck.size() == 0:
-		deck_sprite.visible = false
-		deck_left_num_text.visible = false
+		self.visible = false
 		return
 	
 	# Draw card there is still card left
