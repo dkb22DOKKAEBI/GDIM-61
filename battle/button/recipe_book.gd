c629648f3de684b780538539eb1463f1d5f321f7
MOBIUS <yuchenliu0710@outlook.com>
2025-06-08 19:23:22 -0700

    add recipe book background and disable player action when checking recipe boos

--- battle/button/recipe_book.gd
+++ battle/button/recipe_book.gd
@@ -1,14 +1,19 @@
 extends Control
 
 @onready var recipe_book_visible: PanelContainer = $RecipeBookVisible
+@export var background: ColorRect
 
 func _ready() -> void:
 	recipe_book_visible.visible = false
 
 func _on_recipe_book_button_pressed() -> void:
 	AudioManager.play_sound("CLICK")
+	PlayerController.curr_player_status = PlayerController.PLAYER_STATUS.CHECKING_INFO
 	recipe_book_visible.visible = true
+	background.visible = true
 	
 func _on_exit_book_pressed() -> void:
 	AudioManager.play_sound("CLICK")
+	PlayerController.curr_player_status = PlayerController.PLAYER_STATUS.IDLE
 	recipe_book_visible.visible = false
+	background.visible = false
